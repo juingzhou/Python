99a13d72f1bbb96acf316893f82d60009fc14859
juingzhou <juingzhou@163.com>
2018-05-20 16:16:50 +0800

    Co-Authored-By: juingzhou <juingzhou@users.noreply.github.com>

--- testalp.py
+++ testalp.py
@@ -1,18 +1,18 @@
 import twisted
 import os
+
 s = "helloworld"
-result={}
+result = {}
 for i in s:
     result[i] = s.count(i)
 print(result)
 
-
-result1={}
+result1 = {}
 for j in s:
     if j in result1:
-        result1[j]+=1
+        result1[j] += 1
     else:
-        result1[j]=1
+        result1[j] = 1
 print(result1)
 if os.name == 'nt':
     print("Windows")
--- 查找文件.py
+++ 查找文件.py
@@ -1,15 +1,14 @@
 import os
 
 
-def find_file(parent_dir,file_name):
-    file_abspath = os.path.join(parent_dir,file_name)
+def find_file(parent_dir, file_name):
+    file_abspath = os.path.join(parent_dir, file_name)
     if os.path.isdir(file_abspath):
         for f in os.listdir(file_abspath):
-            find_file(file_abspath,f)
+            find_file(file_abspath, f)
     else:
         if file_abspath.endswith(".py"):
             print(file_abspath)
-find_file("F:\Python","Python")
 
 
-
+find_file("F:\\", "Python")
