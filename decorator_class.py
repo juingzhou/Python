ee6596565f1c0f3190683673ae6786ab54bf6368
“ <juingzhou@163.com>
2018-05-28 15:08:24 +0800

    zjj
    Signed-off-by: “ <juingzhou@163.com>

--- decorator_class.py
+++ decorator_class.py
@@ -10,16 +10,16 @@ class deco(object):
     def __init__(self, func):
         print("---初始化---")
         print("func name is %s" % func.__name__)
-        self.__func = func
+        self.func = func
 
     def __call__(self):
         print("---装饰器的功能---")
-        self.__func()
+        self.func()
 
 
-@deco
+@deco  # 调用并生成Test的一个对象，所以会调用__init__方法，比并且把下面装饰的函数作为参数传进去
 def test():
     print("---test---")
 
 
-test()
+test()  # 调用Test的一个对象的__call__方法
