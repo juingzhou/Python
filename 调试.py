c711f6e6126d404019f6caedfb6a565d82a7e44e
“ <juingzhou@163.com>
2018-05-29 22:17:33 +0800

    zjj
    Signed-off-by: “ <juingzhou@163.com>

---
+++ 调试.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/5/28 15:58
+# @Author : juingzhou
+# @Site : 
+# @File : 调试.py
+# @Software: PyCharm
+
+#pdb调试
+#执行时调试  python -m pdb 文件名.py
+#交互调试
+
+import pdb
